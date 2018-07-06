c32d86bc0ecf8eb8bf9dea2302db223a3e5e2535
George Nychis <gnychis@gmail.com>
2018-07-05 23:05:16 -0400

    updating cmake lists for cuda support

--- train.cpp
+++ train.cpp
@@ -5,34 +5,46 @@
 #include <opencv2/highgui/highgui.hpp>
 
 #include "train.h"
+#include "dir.h"
 
 using namespace std;
 using namespace cv;
 
 
 vector<TrainedImage> load_training_images(string training_dir) {
-	DIR *dir;
 	struct dirent *ent;
 
 	vector<TrainedImage> trained_images;
 
-	if ((dir = opendir(training_dir.c_str())) != NULL) {
-		/* print all the files and directories within directory */
-		while ((ent = readdir(dir)) != NULL) {
+  vector<string> files;
+  int dir_ret = getdir(training_dir, files);
 
-			string img_path(ent->d_name);
-			if (img_path.compare(".") == 0 || img_path.compare("..") == 0)
-				continue;
+  if(dir_ret != 0) {
+    perror("Could not open directory");
+    return trained_images;
+  }
 
-			trained_images.push_back(TrainedImage(training_dir + img_path));
-		}
-		closedir(dir);
-	}
-	else {
-		/* could not open directory */
-		perror("");
-		throw;
-	}
+  for(auto f : files) {
+    cout << "File: " << f << endl;
+
+  } 
+
+	//	/* print all the files and directories within directory */
+	//	while ((ent = readdir(dir)) != NULL) {
+
+	//		string img_path(ent->d_name);
+	//		if (img_path.compare(".") == 0 || img_path.compare("..") == 0)
+	//			continue;
+
+	//		trained_images.push_back(TrainedImage(training_dir + img_path));
+	//	}
+	//	closedir(dir);
+	//}
+	//else {
+	//	/* could not open directory */
+	//	perror("");
+	//	throw;
+	//}
 
 	return trained_images;
 }
