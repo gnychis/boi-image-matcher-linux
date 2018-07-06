1bcc31f4ce597d9944f39bcc0d1b84e07450a705
George Nychis <gnychis@gmail.com>
2018-07-05 23:07:43 -0400

    updating the ignore and adding more linux support

---
+++ BOIImageMatcherConfig.h
@@ -0,0 +1,4 @@
+// the configured options and settings for Tutorial
+#define MATCHER_VERSION_MAJOR 1
+#define MATCHER_VERSION_MINOR 0
+#define MATCHER_VERSION_PATCH 0
---
+++ dir.h
@@ -0,0 +1,3 @@
+#pragma once
+
+int getdir (string dir, vector<string> &files);
